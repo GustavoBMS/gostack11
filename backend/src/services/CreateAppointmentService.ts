f599634ff9dd953c96e4b4e57bdeb54420b4055d
Gustavo Santos <gustavo.santos@keyrus.com.br>
2021-07-02 17:43:55 -0300

    Classe de Erro

--- backend/src/services/CreateAppointmentService.ts
+++ backend/src/services/CreateAppointmentService.ts
@@ -4,6 +4,7 @@ import { getCustomRepository } from 'typeorm';
 import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
+import AppError from '../errors/AppError';
 interface Request {
   provider_id: string;
   date: Date;
