5e2924aa3ec09542ae33d819f49fa86882100163
Gustavo Santos <gustavo.santos@keyrus.com.br>
2021-07-15 14:56:05 -0300

    Nova build e inicio de login

--- frontend-web/src/components/Button/index.tsx
+++ frontend-web/src/components/Button/index.tsx
@@ -1,9 +1,9 @@
 import React, { ButtonHTMLAttributes } from 'react'
 
+import { Container } from './styles';
+
 // Quando nao for alterar nada pode usar type ao inves de interface
 type ButtonsProps = ButtonHTMLAttributes<HTMLButtonElement>;
-
-import { Container } from './styles';
 
 const Button: React.FC<ButtonsProps> = ({ children, ...rest }) => {
   return (
--- frontend-web/src/pages/SignIn/index.tsx
+++ frontend-web/src/pages/SignIn/index.tsx
@@ -14,14 +14,17 @@ import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
+interface SignInFormData {
+  email: string;
+  password: string;
+}
+
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
   const { signIn } = useContext(AuthContext);
 
-  console.log(signIn);
-
-  const handleSubmit = useCallback(async (data: object) => {
+  const handleSubmit = useCallback(async (data: SignInFormData) => {
     try {
       formRef.current?.setErrors({});
 
@@ -34,7 +37,10 @@ const SignIn: React.FC = () => {
         abortEarly: false,
       });
 
-      signIn();
+      signIn({
+        email: data.email,
+        password: data.password,
+      });
     } catch (err) {
       const errors = getValidationErrors(err);
 
