97a8374be7d339067312aa8aa886fdac11a94a24
Gustavo Santos <gustavo.santos@keyrus.com.br>
2021-07-15 18:33:16 -0300

    Estilizacao de toast

--- frontend-web/src/App.tsx
+++ frontend-web/src/App.tsx
@@ -4,6 +4,7 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
 
+import ToastContainer from './components/ToastContainer';
 import { AuthProvider } from './hooks/AuthContext';
 
 function App() {
@@ -12,6 +13,9 @@ function App() {
       <AuthProvider>
         <SignIn />
       </AuthProvider>
+
+      <ToastContainer />
+
       <GlobalStyle />
     </>
   );
