064d8931037957604d75398ab8ae47a9fb1cc48f
Gustavo Santos <gustavo.santos@keyrus.com.br>
2021-07-22 20:51:56 -0300

    Rota de imagem

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -13,11 +13,11 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('./', express.static(uploadConfig.directory));
+app.use('./files', express.static(uploadConfig.directory));
 app.use(routes);
 
-app.use((err: Error, request:Request, response:Response, next:NextFunction) => {
-  if(err instanceof AppError){
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
     return response.status(err.statusCode).json({
       status: 'error',
       message: err.message,
